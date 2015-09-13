e648c3e4f126a81d572a5d11d311fb79565bfe7f
Andrei Solntsev <andrei.solntsev@gmail.com>
2015-09-13 13:57:27 +0200

    update

--- src/test/java/selenide/LoginPage.java
+++ src/test/java/selenide/LoginPage.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class LoginPage {
   public void login(String username, String password) {
     open("https://www.google.com/accounts/ServiceLogin?service=code");
-    $(By.name("Email")).val(username);
+    $(By.name("Email")).val(username).submit();
     $(By.name("Passwd")).val(password).submit();
   }
 }
